b4cd9bdd3e6af4814e19da435152b24f9a4cdf74
fs-s <55434294+fs-s@users.noreply.github.com>
2020-09-23 14:40:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<?php
+
+require_once 'connect.php';
+
+$stmt = $pdo->prepare('SELECT * FROM books');
+$stmt->execute();
+$aBooks = $stmt->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    <?php
+        foreach ($aBooks as $book) {
+            echo '<li><a href="book.php?id=' . $book['id'] . '">' . $book['title'] . '</a></li>';
+        }
+    ?>
+    </ul>
+</body>
+</html>
